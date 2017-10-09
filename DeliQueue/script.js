8e97876d0d94a1fcb35cd5187d2637d1d0b46214
Ashanti Pratt <ashantipratt@Ashantis-MacBook-Air.local>
2017-10-08 23:16:32 -0400

    Committing changes to deli counter project

--- DeliQueue/script.js
+++ DeliQueue/script.js
@@ -20,54 +20,52 @@
 
 				//push values into array
 				queue.push({
-				"name": custName, 
-				"order": custOrder
+					"name": custName, 
+					"order": custOrder
 				});
 				
 				// cell content in the DOM
-				rowCell1.innerHTML = queue.length
+				rowCell1.innerHTML = queue.length;
 				rowCell2.innerHTML = custName;
 				rowCell3.innerHTML = custOrder;
 
-				// clears display message above table
-				document.getElementById("message").innerHTML = ""
+				// clears display messages
+				document.getElementById("message").innerHTML = "";
+				document.getElementById("emptyQueue").innerHTML = "";
 			
 				// clear input text box onclick
 				document.getElementById("custName").value = "";
 				document.getElementById("custOrder").value = "";
 				
-			}
+			};
 
 			// create customer served function
 			function custServed(){
-
+				
 				var table = document.getElementById("queueTable");
+				var rows = document.getElementsByTagName("tr");
 
 				//remove customer from queue
 				table.deleteRow(1);
 
 
-				//display who was served in the DOM. Final message not displaying 
-				if(queue.length > 1){
+				//display who was served in the DOM. 
+				if(queue.length > 0){
 					document.getElementById("message").innerHTML = queue[0].name + " was served " + queue[0].order;	
+					
 					// remove object from front of the array
 					queue.shift();
-					
-
-				}else{
-					document.getElementById("message").innerHTML = "";
-					document.getElementById("message").innerHTML = queue[0].name + " was served " + queue[0].order;	
-
-					document.getElementById("emptyQueue").innerHTML = "There is no one waiting in line.";
-				}
-
-				
-	
+				};
 
 				// display new customer position 
-				var rows = document.getElementsByTagName("tr");
 				for(var i = 1; i < rows.length; i++){
 
 					rows[i].children[0].innerHTML = rows[i].rowIndex;
-				} 
-			}
+				};
+
+				// display message if queue is empty
+				if(rows.length == 1){
+					//document.getElementById("message").innerHTML = "";
+					document.getElementById("emptyQueue").innerHTML = "There is no one waiting in line";
+				}; 
+			};
